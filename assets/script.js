d936e8f2284a7750d3528e75a4abf5e054cbca32
SamyBidar2 <samybidarpro@gmail.com>
2024-01-04 21:36:55 +0100

    premiers tests caroussel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,35 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+console.log(slides.length);
+image=document.getElementById("ImageBaniere");
+texte=document.getElementById("TexteBanniere");
+// let CompteurDroite=0;
+let NumeroSlide=0;
+let Gauche= -1;
+let Droite= 1;
+
+// function droite(){
+// 	console.log("clic droite");
+// 	CompteurDroite=CompteurDroite+1;
+
+// 	image.src="./assets/images/slideshow/" + slides[CompteurDroite].image;
+
+// }
+
+function ChangeSlide(sens){
+	console.log("clic");
+
+	//permet de faire la boucle lorsqu'on arrive au dernier slide
+	NumeroSlide = NumeroSlide + sens;
+	if(NumeroSlide > slides.length -1)
+		NumeroSlide = 0;
+
+	if(NumeroSlide < 0)
+		NumeroSlide = slides.length -1;
+
+	console.log(NumeroSlide);
+	image.src="./assets/images/slideshow/" + slides[NumeroSlide].image;
+	texte.content= slides[NumeroSlide].tagLine;
+
+}
