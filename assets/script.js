984c945109c91ef04ebd367406c8b8a17adb837a
SamyBidar2 <samybidarpro@gmail.com>
2024-01-07 23:25:10 +0100

    ajout boutons + fonction droite

--- assets/script.js
+++ assets/script.js
@@ -16,35 +16,65 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Création d'un élément Div et ajout de la classe
+for(var i=0; i<slides.length; i++){
+    // Récupérer l'élément avec la classe "dots"
+    var dotsContainer = document.querySelector('.dots');
+
+    // Créer une nouvelle balise <div> avec la classe "dot"
+    var newDot = document.createElement('div');
+    newDot.className = 'dot';
+
+	// Si on est sur le premier élément, alors on lui ajoute la classe 'dot_selected'	
+	if(i === 0){
+		newDot.classList.add('dot_selected');
+	}
+
+    // Ajouter la nouvelle balise <div> à l'élément avec la classe "dots"
+    dotsContainer.appendChild(newDot);
+}
+
+
+
 console.log(slides.length);
 image=document.getElementById("ImageBaniere");
 texte=document.getElementById("TexteBanniere");
-// let CompteurDroite=0;
-let NumeroSlide=0;
-let Gauche= -1;
-let Droite= 1;
 
-// function droite(){
-// 	console.log("clic droite");
-// 	CompteurDroite=CompteurDroite+1;
+let CompteurDroite=0;
 
-// 	image.src="./assets/images/slideshow/" + slides[CompteurDroite].image;
+function droite(){
+	console.log("clic droit");
+	document.querySelectorAll('.dots .dot')[CompteurDroite].classList.remove('dot_selected');
+	CompteurDroite=CompteurDroite+1;
 
-// }
-
-function ChangeSlide(sens){
-	console.log("clic");
-
-	//permet de faire la boucle lorsqu'on arrive au dernier slide
-	NumeroSlide = NumeroSlide + sens;
-	if(NumeroSlide > slides.length -1)
-		NumeroSlide = 0;
-
-	if(NumeroSlide < 0)
-		NumeroSlide = slides.length -1;
-
-	console.log(NumeroSlide);
-	image.src="./assets/images/slideshow/" + slides[NumeroSlide].image;
-	texte.content= slides[NumeroSlide].tagLine;
-
+	image.src="./assets/images/slideshow/" + slides[CompteurDroite].image;
+	texte.innerHTML=slides[CompteurDroite].tagLine;
+	document.querySelectorAll('.dots .dot')[CompteurDroite].classList.add('dot_selected');
 }
+
+
+
+
+// Test fonction avec argument
+
+// let NumeroSlide=0;
+// let Gauche= -1;
+// let Droite= 1;
+
+// function ChangeSlide(sens){
+// 	console.log("clic");
+	
+// 	//permet de faire la boucle lorsqu'on arrive au dernier slide
+// 	NumeroSlide = NumeroSlide + sens;
+// 	if(NumeroSlide > slides.length -1)
+// 		NumeroSlide = 0;
+
+// 	if(NumeroSlide < 0)
+// 		NumeroSlide = slides.length -1;
+
+
+// 	console.log(NumeroSlide);
+// 	image.src="./assets/images/slideshow/" + slides[NumeroSlide].image;
+// 	texte.innerHTML=slides[CompteurDroite].tagLine;
+// }
